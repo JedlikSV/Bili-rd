6c164912910aba12204931c0d30fa2829a984bcb
suslecz.viktoria <suslecz.viktoria@C102-15>
2020-02-18 13:45:17 +0100

    A kiíró eljárás elkészült.

--- Biliárd/Program.cs
+++ Biliárd/Program.cs
@@ -12,11 +12,27 @@ namespace Biliárd
         {
             List<int> golyók = new List<int>();
             Kezdés(golyók);
-            for (int i = 0; i < golyók.Count; i++)
-            {
-                Console.WriteLine(golyók[i]);
-            }
+            int leeső = LeesőSorsoló(golyók);
+            Kiíró(golyók);           
+
             Console.ReadKey();
+        }
+
+        static void Kiíró(List<int> golyók)
+        {
+            foreach (int item in golyók)
+            {
+                Console.WriteLine($"{item} ");
+            }
+        }
+
+        static int LeesőSorsoló(List<int> golyók)
+        {
+            Random rnd = new Random();
+            int szám;
+            szám = rnd.Next(golyók.Count);
+            golyók.Remove(szám);
+            return golyók[szám];
         }
 
         static void Kezdés(List<int> golyók)
